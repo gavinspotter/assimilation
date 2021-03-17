260473d195297de6aed72b8c300557d09bd09720
Osiris <potterosiris@gmail.com>
2021-03-17 16:51:05 -0400

    words schema

--- assim-server/models/Words.js
+++ assim-server/models/Words.js
@@ -1,3 +1,11 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const wordsSchema = new Schema({
+    word: { type: String, required: true },
+    defintion: [{ type: String }],
+    phonicWords: [{ type: mongoose.Types.ObjectId, required: true, ref: "Words" }]
+
+})
+
